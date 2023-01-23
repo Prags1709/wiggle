8891e2b4a2607430af3c45adb569b2ba38878f91
Prags1709 <prags1709@gmail.com>
2023-01-23 12:27:50 +0530

    FD

---
+++ script/register.js
@@ -0,0 +1,74 @@
+let logo = document.querySelector("#logo")
+ logo.addEventListener("click",()=>{
+    window.location.href = "index.html"
+ })
+
+ let reg = document.querySelector("#register")
+ reg.addEventListener("click",()=>{
+    window.location.href = "register.html"
+ })
+
+ let signin = document.querySelector("#signin")
+ signin.addEventListener("click",()=>{
+    window.location.href = "signin.html"
+ })
+
+ let women = document.querySelector("#women")
+ women.addEventListener("click",()=>{
+    window.location.href = "women.html"
+ })
+
+ let men = document.querySelector("#men")
+ men.addEventListener("click",()=>{
+    window.location.href = "men.html"
+ })
+
+ let cart_page = document.querySelector("#cart_bag")
+ cart_page.addEventListener("click",()=>{
+   window.location.href = "cart.html"
+ })
+
+ let Admin = document.querySelector("#admin")
+ Admin.addEventListener("click",()=>{
+   window.location.href = "a-login.html"
+ })
+
+ let apply = document.querySelector("#apply")
+ apply.addEventListener("click",async ()=>{
+    let first_Name = document.querySelector("#firstname").value ;
+    let last_Name = document.querySelector("#lastname").value;
+    let Phone = document.querySelector("#phone").value;
+    let Gender = document.querySelector("#gender").value;
+    let Dob = document.querySelector("#dob").value;
+    let Email = document.querySelector("#email").value;
+    let Password = document.querySelector("#password").value;
+
+    let user_data = {
+        first_name: first_Name,
+        last_name: last_Name,
+        phone: Phone,
+        gender: Gender,
+        DOB: Dob,
+        email: Email,
+        pass: Password
+    }
+
+    try {
+        let res = await fetch("http://localhost:2023/users/register", {
+            method: 'POST',
+            headers: {
+            'content-type': 'application/json'
+            },
+            body: JSON.stringify(user_data)
+        })
+
+        if(res.ok==true){
+            alert(`Hello ${user_data.first_name}, You are Registration Successfully Completed`)
+            window.location.href = "index.html"
+        }
+    } catch (error) {
+      alert("Something Went wrong")
+        console.log(error);
+    }
+
+ })
---
+++ script/signin.js
@@ -0,0 +1,74 @@
+let logo = document.querySelector("#logo")
+ logo.addEventListener("click",()=>{
+    window.location.href = "index.html"
+ })
+
+ let reg = document.querySelector("#register")
+ reg.addEventListener("click",()=>{
+    window.location.href = "register.html"
+ })
+
+ let signin = document.querySelector("#signin")
+ signin.addEventListener("click",()=>{
+    window.location.href = "signin.html"
+ })
+
+ let regi = document.querySelector("#create")
+ regi.addEventListener("click",()=>{
+    window.location.href = "register.html"
+ })
+
+ let women = document.querySelector("#women")
+ women.addEventListener("click",()=>{
+    window.location.href = "women.html"
+ })
+
+ let men = document.querySelector("#men")
+ men.addEventListener("click",()=>{
+    window.location.href = "men.html"
+ })
+
+ let cart_page = document.querySelector("#cart_bag")
+ cart_page.addEventListener("click",()=>{
+   window.location.href = "cart.html"
+ })
+
+ let Admin = document.querySelector("#admin")
+ Admin.addEventListener("click",()=>{
+   window.location.href = "a-login.html"
+ })
+
+ let  sign_IN= document.querySelector("#sign_in")
+ sign_IN.addEventListener("click",async ()=>{
+    let Email = document.querySelector("#email").value;
+    let Password = document.querySelector("#password").value;
+
+    let user_data = {
+        email: Email,
+        pass: Password
+    }
+
+    //console.log(user_data);
+    try {
+        let res = await fetch("http://localhost:2023/users/login", {
+            method: 'POST',
+            headers: {
+            'content-type': 'application/json'
+            },
+            body: JSON.stringify(user_data)
+        })
+
+        let data =await res.json()
+        localStorage.setItem("token",data.token)
+         console.log(data)
+
+        if(res.ok==true){
+            alert(`Welcome, You are Signin Successfully Completed`)
+            window.location.href = "index.html"
+        }
+    } catch (error) {
+         alert("Wrong Credential")
+        console.log(error);
+    }
+
+})   
